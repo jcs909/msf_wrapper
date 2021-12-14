ac111ae124c9db1b30a578abb61b4c32edc15d9d
jcs909 <87347137+jcs909@users.noreply.github.com>
2021-12-13 18:25:42 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3                # specify the interpreter
+
+
+### IMPORT STATEMENTS ###
+import sys  # import the sys library
+
+
+### HELPER FUNCTIONS ###
+def is_even(file):  # define our line printing function
+
+    for line in file:  # loop through every line in the file
+
+        line = line.rstrip()  # remove the trailing newlines
+
+        for nums in line:  # loop through each line
+
+            print(word)  # print each word
+
+
+### MAIN FUNCTION ###
+def main():  # define a main function
+
+    file_to_open = sys.argv[1]  # get the file name from the cmd line
+
+    opened_file = open(file)  # open the file
+
+    print_words(opened_file)  # call our print words function with the open file
+
+    opened_file.close()  # close the file when done
+
+
+### DUNDER CHECK ###
+if __name__ == '__main__':  # dunder check
+    main()  # call the main function
