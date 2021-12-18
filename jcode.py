5e15aed6fe6687627fc140a29174178ed0e3aa4a
Jaime Serranzana <jcs909@gmail.com>
2021-12-18 15:18:56 -0500

    getting local ip

--- jcode.py
+++ jcode.py
@@ -21,7 +21,9 @@ def windows_reverse_payload():
     print("output:", return_code)
 
 def get_ip():
-    print(socket.gethostbyname(socket.gethostname()))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    print(s.getsockname()[0])
 
 
 ### MAIN FUNCTION ###
