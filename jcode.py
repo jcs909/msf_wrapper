36557628a03fb896469e39cf3b4bdcd8bf6a89b5
Jaime Serranzana <jcs909@gmail.com>
2021-12-18 13:17:47 -0500

    jcode

---
+++ jcode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3                # specify the interpreter
+
+
+### IMPORT STATEMENTS ###
+import sys  # import the sys library
+import subprocess
+
+
+### HELPER FUNCTIONS ###
+def get_local():  # define our line printing function
+    return_code = subprocess.call(['localhost'])
+    print("output:", return_code)
+
+### MAIN FUNCTION ###
+def main():  # define a main function
+    get_local()
+
+    # file_to_open = sys.argv[1]  # get the file name from the cmd line
+
+    # opened_file = open(file_to_open)  # open the file
+
+    # is_even(opened_file)  # call our print words function with the open file
+
+    # opened_file.close()  # close the file when done
+
+
+### DUNDER CHECK ###
+if __name__ == '__main__':  # dunder check
+    main()  # call the main function
