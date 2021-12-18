bb7e9cf860ea52e4103c0649877079d532388b56
Gavyn Welsh <87346650+GavynWelsh@users.noreply.github.com>
2021-12-18 13:35:29 -0500

    Add files via upload

---
+++ Msfwrapper.py
@@ -0,0 +1,33 @@
+#Varibles/Imports
+import os
+from colorama import Fore, Back, Style
+import subprocess
+
+#Main Script
+#Varibles
+pastPackets = ()
+
+#allowable operating systems
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+operatingSystem = "Linux", "Windows", "Mac"
+
+#Asking the user the target's operating system
+print(Fore.RED + "MSFVenom Wrapper:")
+#Main Menu
+print("Welcome to our MSFVenom wrapper that will help you through creating a packet with step by step questions guided.")
+print()
+print("   Options:  ")
+print()
+print("   C - Create New Packet ")
+print("   P - Past Packet ")
+print()
+
+userInput = input(Fore.WHITE + "Command Line: ")
+
+#Testing if the operation system is a valid system if not loop until right
+while userInput not in operatingSystem:
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(str(userInput) + " is not a valid operating system, what is your target's Operating System?")
+  print("Options: (Linux, Windows, Mac)")
+  userInput = input()
+  
